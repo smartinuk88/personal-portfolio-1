ae1af78692afcc908b2c4298441ebfcefe0a722c
Scott Martin <scott.martin88@live.com>
2024-06-17 12:45:17 +0200

    About me text

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,34 +26,47 @@ function Header() {
           </div>
           <div
             style={{ display: display }}
-            className="text-md dark:text-dm-support w-full pt-4"
+            className="text-xs md:text-small dark:text-dm-support w-full pt-4"
           >
             <p className="mb-2">
-              Hi, I'm <span className="font-semibold">Scott</span>.
+              Hi, I'm <span className="font-bold">Scott</span>,
             </p>
             <p className="mb-2">
-              Thanks for coming. Make yourself comfortable.
-            </p>
-
-            <p className="mb-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-              placeat minus porro fugit facilis, labore saepe delectus quaerat
-              beatae quas similique odio. Reprehenderit error exercitationem
-              delectus facilis debitis expedita sit.
+              a passionate front-end web developer living in the vibrant city of
+              Murcia, Spain. My journey into the world of web development is
+              fueled by a blend of creativity and technical curiosity, which I
+              honed through years of experience as a photographer and web
+              administrator.
             </p>
 
             <p className="mb-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Quibusdam dolorem iure corrupti sapiente. Commodi, blanditiis
-              sunt.
+              Driven by a love for visual storytelling, my background in
+              photography allows me to bring a unique aesthetic perspective to
+              my development projects, emphasizing clean, user-friendly
+              interfaces that engage and communicate clearly. Transitioning to
+              web development was a natural progression for me, combining my
+              keen visual eye with technical skills to build seamless digital
+              experiences.
+            </p>
+
+            <p className="mb-2">
+              As a self-taught developer, I am committed to continuous learning
+              and improvement. I thrive on challenges and enjoy staying updated
+              with the latest industry trends and technologies. My current
+              toolkit includes React, TailwindCSS, and Firebase, with a keen
+              interest in expanding to full-stack development.
             </p>
             <p className="mb-2">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit
-              dolores minus excepturi neque.
+              Whether collaborating on projects or crafting individual sites, my
+              goal is to create websites that not only look great but also
+              provide functional, intuitive user experiences. I am eager to
+              bring my creative flair and meticulous eye for detail to a dynamic
+              team, where I can contribute to impactful and genuinely useful
+              projects and grow alongside like-minded professionals.
             </p>
             <p className="mb-2">
               <a
-                className="inline-block font-semibold transition-all duration-400 hover:opacity-80 text-lm-fucshia dark:text-dm-fucshia"
+                className="inline-block bold transition-all duration-400 hover:opacity-80 text-lm-fucshia dark:text-dm-fucshia"
                 aria-label="CV on Google Drive"
                 rel="noopener"
                 target="_blank"
