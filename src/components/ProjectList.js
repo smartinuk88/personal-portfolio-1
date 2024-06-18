3e698802a59e90a04a17364900171d467b88d336
Scott Martin <scott.martin88@live.com>
2024-06-18 12:12:06 +0200

    Input data for 2nd portfolio project

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,11 +1,68 @@
 import ProjectRow from "./ProjectRow";
+import flashFluentImg1 from "../images/flashfluent-1.png";
+import amazonImg1 from "../images/amazon-1.png";
+import { FaReact } from "react-icons/fa";
+import {
+  SiFirebase,
+  SiTailwindcss,
+  SiReactrouter,
+  SiTypescript,
+  SiNextdotjs,
+} from "react-icons/si";
+
+const flashFluentBullets = [
+  {
+    header: "Firebase Integration",
+    body: "Secure authentication and real-time CRUD operations with Firestore for dynamic content management.",
+  },
+  {
+    header: "Efficient Data Syncing",
+    body: "Implements debouncing and a 90-second interval for robust data synchronization, optimizing server interactions.",
+  },
+  {
+    header: "Streak Functionality & Context API",
+    body: "Tracks user engagement with a motivational streak system and utilizes React Context for streamlined state management.",
+  },
+];
+
+const amazonBullets = [
+  {
+    header: "Advanced Web Scraping Integration",
+    body: "Utilizes Oxylabs to scrape ecommerce data directly from Amazon, ensuring up-to-date product information and search functionality, integrated seamlessly with Next.js 14 for optimal performance.",
+  },
+  {
+    header: "Enhanced State Management with Zustand",
+    body: "Leverages Zustand for efficient global state management across the application, allowing for a smooth and responsive user interface that handles complex state transitions effortlessly.",
+  },
+  {
+    header: "Optimized Server-Side Caching",
+    body: "Implements server-side data caching techniques in Next.js 14 to speed up content delivery, enhancing the overall user experience with faster load times and reduced server load.",
+  },
+  {
+    header: "Dynamic Routing and UX Design",
+    body: "Employs advanced Next.js routing capabilities, including parallel and intercepting routes, to ensure a seamless shopping experience, allowing users to manage their baskets without interrupting their browsing flow.",
+  },
+];
 
 function ProjectList() {
   return (
     <div className="flex flex-col gap-16 mb-20">
-      <ProjectRow />
-      <ProjectRow />
-      <ProjectRow />
+      <ProjectRow
+        title="FlashFluent"
+        bullets={flashFluentBullets}
+        image={flashFluentImg1}
+        techIcons={[FaReact, SiFirebase, SiTailwindcss, SiReactrouter]}
+        codeLink="https://github.com/smartinuk88/flashcard-app"
+        liveLink="https://main--stunning-cupcake-ed88e2.netlify.app/"
+      />
+      <ProjectRow
+        title="Amazon Redesign w/ Oxylabs Webscraper"
+        bullets={amazonBullets}
+        image={amazonImg1}
+        techIcons={[SiNextdotjs, SiTypescript, SiTailwindcss]}
+        codeLink="https://github.com/smartinuk88/amazon-redesign-oxylabs-scraper"
+        liveLink="https://main--stunning-cupcake-ed88e2.netlify.app/"
+      />
     </div>
   );
 }
--- src/components/ProjectRow.js
+++ src/components/ProjectRow.js
@@ -1,61 +1,40 @@
-import { useState } from "react";
-import projectImg from "../images/flashfluent-1.png";
-import hoverImg from "../images/flashfluent-2.png";
-import { FaReact } from "react-icons/fa";
-import { SiFirebase } from "react-icons/si";
-import { SiTailwindcss } from "react-icons/si";
-import { SiReactrouter } from "react-icons/si";
-
-function ProjectRow({
-  image,
-  secondImage,
-  title,
-  bullets,
-  tech,
-  codeLink,
-  liveLink,
-}) {
-  const [imgSrc, setImgSrc] = useState(projectImg);
+function ProjectRow({ image, title, bullets, techIcons, codeLink, liveLink }) {
+  const colors = [
+    "text-lm-fucshia dark:text-dm-fucshia",
+    "text-lm-mint dark:text-dm-mint",
+    "text-lm-custom-orange dark:text-dm-custom-orange",
+  ];
+  const getColor = (index) => {
+    return colors[index % colors.length];
+  };
 
   return (
-    <div className="lg:h-auto w-full border-2 border-lm-custom-black dark:border-dm-light shadow-md  items-center">
-      <div className="flex flex-col lg:flex-row text-center">
+    <div className="xl:h-auto w-full border-2 border-lm-custom-black dark:border-dm-light shadow-md  items-center">
+      <div className="flex flex-col xl:flex-row text-center">
         <img
-          className="object-cover aspect-w-16 aspect-h-9 w-full h-auto lg:w-1/2"
-          src={projectImg}
-          alt=""
+          className="aspect-w-16 aspect-h-9 w-full h-auto object-contain xl:w-1/2"
+          src={image}
+          alt={`${title} project screenshot`}
         />
 
-        <div className="flex flex-col justify-between px-4 py-6 leading-normal lg:w-1/2">
+        <div className="flex flex-col justify-between px-4 py-6 leading-normal xl:w-1/2">
           <h2 className="mb-4 text-2xl font-bold tracking-tight text-center text-lm-custom-black dark:text-dm-light">
-            FlashFluent
+            {title}
           </h2>
 
           <div className="flex flex-col text-left space-y-2 text-xs lg:text-md text-lm-support dark:text-dm-support mb-6">
-            <p>
-              <span className="font-bold">Firebase Integration:</span> Secure
-              authentication and real-time CRUD operations with Firestore for
-              dynamic content management.
-            </p>
-            <p>
-              <span className="font-bold">Efficient Data Syncing:</span>{" "}
-              Implements debouncing and a 90-second interval for robust data
-              synchronization, optimizing server interactions.
-            </p>
-            <p>
-              <span className="font-bold">
-                Streak Functionality & Context API:
-              </span>{" "}
-              Tracks user engagement with a motivational streak system and
-              utilizes React Context for streamlined state management.
-            </p>
+            {bullets.map((bullet) => (
+              <p>
+                <span className="font-bold">{bullet.header}</span>:{" "}
+                {bullet.body}
+              </p>
+            ))}
           </div>
 
           <div className="flex justify-between w-2/5 mx-auto text-4xl pb-4">
-            <FaReact className="text-lm-fucshia dark:text-dm-fucshia" />
-            <SiTailwindcss className="text-lm-mint dark:text-dm-mint" />
-            <SiFirebase className="text-lm-custom-orange dark:text-dm-custom-orange" />
-            <SiReactrouter className="text-lm-fucshia dark:text-dm-fucshia" />
+            {techIcons.map((IconComponent, index) => (
+              <IconComponent key={index} className={getColor(index)} />
+            ))}
           </div>
         </div>
       </div>
@@ -63,7 +42,7 @@ function ProjectRow({
       <div className="flex w-full h-16 justify-between text-center border-t-2 border-lm-custom-black dark:border-dm-light">
         <div className="text-lg w-full h-full border-r border-lm-custom-black dark:border-dm-light">
           <a
-            href="https://main--stunning-cupcake-ed88e2.netlify.app/"
+            href={liveLink}
             rel="noopener"
             target="_blank"
             className="flex justify-center items-center w-full h-full hover:opacity-80 transition-all duration-500 text-lm-custom-black dark:text-lm-light hover:text-lm-light dark:hover:text-dm-custom-black hover:bg-lm-custom-black dark:hover:bg-dm-light"
@@ -73,7 +52,7 @@ function ProjectRow({
         </div>
         <div className="text-lg w-full h-full border-l border-lm-custom-black dark:border-dm-light">
           <a
-            href="https://github.com/smartinuk88/flashcard-app"
+            href={codeLink}
             rel="noopener"
             target="_blank"
             className="flex justify-center items-center w-full h-full hover:opacity-80 transition-all duration-500 text-lm-custom-black dark:text-dm-light hover:text-lm-light dark:hover:text-dm-custom-black hover:bg-lm-custom-black dark:hover:bg-dm-light"
